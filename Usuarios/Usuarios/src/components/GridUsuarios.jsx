9326414c9a92ee11ee113c8e9861738177805106
David Peralta <19470212@campeche.tecnm.mx>
2022-09-09 21:17:14 -0500

    deucumentacion agregada

--- Usuarios/Usuarios/src/components/GridUsuarios.jsx
+++ Usuarios/Usuarios/src/components/GridUsuarios.jsx
@@ -26,9 +26,26 @@ export const GridUsuarios = ({limiteUsuarios}) => {
     <>
         <h1>Usuarios Random </h1>
         <div className='grid'>
-
-
+        {
+        //se le pasa el contesto a la lista del dtusr
+        }
           <ListaUsuarios dtusr={dtusr}/>
+          {
+        /*
+        {
+        //map para recorrer el objeto recibido de helpers
+        //dentro de los parentecis se pinta el resultado del recorrido
+        dtusr.results.map(usuario => (
+            <div key={usuario.login.uuid} usuario={usuario}>
+                <img src={usuario.picture.medium} />
+                <div>
+                  <p>{usuario.name.first}{usuario.name.last}</p>
+                </div>
+            </div>
+        ))
+        }
+        */
+      }
         </div>
     </>
     
